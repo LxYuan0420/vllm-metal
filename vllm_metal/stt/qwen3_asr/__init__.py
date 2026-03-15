08e7bbd94a2f02872a06c7afcf0ca3ed87353138
Yuan Lik Xun <lxyuan0420@gmail.com>
2026-03-15 11:23:31 +0800

    Refactor STT: package Qwen3-ASR model
    Signed-off-by: Yuan Lik Xun <lxyuan0420@gmail.com>

---
+++ vllm_metal/stt/qwen3_asr/__init__.py
@@ -0,0 +1,26 @@
+# SPDX-License-Identifier: Apache-2.0
+"""Qwen3-ASR STT implementation (model-owned package)."""
+
+from .model import (
+    AudioEncoder,
+    Qwen3ASRAudioConfig,
+    Qwen3ASRConfig,
+    Qwen3ASRModel,
+    Qwen3ASRTextConfig,
+    Qwen3Attention,
+    Qwen3LM,
+    _get_cnn_output_lengths,
+    _get_feat_extract_output_lengths,
+)
+
+__all__ = [
+    "AudioEncoder",
+    "Qwen3ASRAudioConfig",
+    "Qwen3ASRConfig",
+    "Qwen3ASRModel",
+    "Qwen3ASRTextConfig",
+    "Qwen3Attention",
+    "Qwen3LM",
+    "_get_cnn_output_lengths",
+    "_get_feat_extract_output_lengths",
+]
